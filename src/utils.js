7618c180e43e66d7a440f449c91ce77db8f4f9c1
aleksandr <against.vegetables@gmail.com>
2020-08-18 22:16:33 +0300

    render function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,40 @@
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.append(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.append(element);
+      break;
+  }
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`); // 1
+  newElement.innerHTML = template; // 2
+
+  return newElement.firstChild; // 3
+};
+
+
+// todo все, что ниже, вероятно надо перенести в другой файл
+export const RenderPosition = {
+  BEFOREBEGIN: `beforebegin`,
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`,
+};
+
 export const dateFormatting = (date) => {
   const MONTHS = [
     `JAN`,
@@ -24,3 +61,4 @@ export const createDateInAttributeFormat = (date) => {
   const day = date.getDate();
   return `${year}-${month}-${day}`;
 };
+
