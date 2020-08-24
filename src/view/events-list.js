73f9708d262319ce6777770a98fcd9b786da5651
aleksandr <against.vegetables@gmail.com>
2020-08-24 23:16:03 +0300

    Создал абстрактный класс, переписал простые компоненты

--- src/view/events-list.js
+++ src/view/events-list.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createEventsListTemplate = () => {
   return (
@@ -7,23 +7,8 @@ const createEventsListTemplate = () => {
   );
 };
 
-export default class EventsList {
-  constructor() {
-    this._element = null;
-  }
-
+export default class EventsList extends AbstractView {
   getTemplate() {
     return createEventsListTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
