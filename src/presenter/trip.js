f392a70437295111106ac559ab45a18f66a02363
aleksandr <against.vegetables@gmail.com>
2020-08-25 20:31:08 +0300

    создал презентер trip.js

---
+++ src/presenter/trip.js
@@ -0,0 +1,11 @@
+import SortView from "../view/sort.js";
+import DaysListView from "../view/days-list.js";
+import DayView from "../view/day.js";
+import EventsListView from "../view/events-list.js";
+import EventView from "../view/event.js";
+import EventEditView from "../view/event-edit.js";
+import NoEventView from "../view/no-event.js";
+
+export default class Trip {
+
+}
