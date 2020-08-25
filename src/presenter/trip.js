3df5232ca8ef67f996f0454b8e3f6c1b4f4d7ff9
aleksandr <against.vegetables@gmail.com>
2020-08-26 01:01:20 +0300

    причесао код в презентере, раписал вопросы

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -15,39 +15,43 @@ export default class Trip {
     this._sortComponent = new SortView();
     this._daysListComponent = new DaysListView();
     this._noEventComponent = new NoEventView();
+
+    // todo разобраться с конструктором
+    // вопрос: какие компоненты мы определяем в конструкторе?
+    // сейчас я здесь написал только статичные компоненты
+    // но я не уверен, что это правильный критерий
   }
 
   init(tripDays) {
     this._tripDays = tripDays.slice();
-
     render(this._tripContainer, this._tripComponent, RenderPosition.BEFOREEND);
     this._renderTrip();
-
   }
 
   _renderSort() {
     render(this._tripComponent, this._sortComponent, RenderPosition.BEFOREEND);
   }
+
   _renderDaysList() {
     render(this._tripComponent, this._daysListComponent, RenderPosition.BEFOREEND);
   }
 
   _renderDay(tripDay, index) {
-    const dayComponent = new DayView(tripDay.day, index);
+    const dayComponent = new DayView(tripDay.day, index); // нормально же, что я внутри метода создаю нвоый экземпляр?
     render(this._daysListComponent, dayComponent, RenderPosition.BEFOREEND);
     this._renderEventsList(dayComponent, tripDay.events);
   }
 
-  _renderEventsList(dayComponent, dayEvents) {
+  _renderEventsList(dayElement, dayEvents) {
+    // нормально, что в названии первого параметра есть слово Element?
     const eventsListComponent = new EventsListView();
-    render(dayComponent, eventsListComponent, RenderPosition.BEFOREEND);
+    render(dayElement, eventsListComponent, RenderPosition.BEFOREEND);
     for (let i = 0; i < dayEvents.length; i++) {
       this._renderEvent(eventsListComponent, dayEvents[i]);
     }
   }
 
-
-  _renderEvent(eventsList, event) {
+  _renderEvent(eventsListElement, event) {
     const eventComponent = new EventView(event);
 
     // todo Здесь должна быть вторая структура данных - offers, пофиксить, когда подрубим данные с бэка
@@ -80,7 +84,7 @@ export default class Trip {
       document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
-    render(eventsList, eventComponent, RenderPosition.BEFOREEND);
+    render(eventsListElement, eventComponent, RenderPosition.BEFOREEND);
   }
 
   _renderNoEvent() {
@@ -92,12 +96,16 @@ export default class Trip {
       this._renderNoEvent();
       return;
     }
+
     this._renderSort();
     this._renderDaysList();
 
     for (let i = 0; i < this._tripDays.length; i++) {
       this._renderDay(this._tripDays[i], i);
     }
-
   }
 }
+
+// todo разобрать вопросы
+// нэйминг this._tripComponent = new TripView(); - возможно стоило вместо trip использовать board?
+//
