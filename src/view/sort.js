bbf66015f0061045fb85d2594d0c1dd7ee83bfcc
aleksandr <against.vegetables@gmail.com>
2020-08-27 19:02:37 +0300

    оставил тудушку

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,9 +1,14 @@
 import AbstractView from "./abstract.js";
 
 const createSortTemplate = () => {
+  // todo разобраться с dayTitle
+  // Надо выводить dayTitle только в случае сортировки по умолчанию
+  // И я не придумал как сделать это поприличнее
+  // Возможно в следующих лекциях про связывание данных об этом расскажут
+  const dayTitle = `Day`;
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+      <span class="trip-sort__item  trip-sort__item--day">${dayTitle}</span>
 
       <div class="trip-sort__item  trip-sort__item--event">
         <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
