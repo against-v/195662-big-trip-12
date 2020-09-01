b2b87230f8c3cf9b8c66cb23f32d3a620bdb3fbf
aleksandr <against.vegetables@gmail.com>
2020-09-01 23:11:28 +0300

    обновил утилсы

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,21 +1,8 @@
 import {nanoid} from 'nanoid';
+import {shuffleArray, getRandomInteger} from "../utils/common";
 import {CITIES, EVENT_OFFERS} from "../const.js";
 
 const ID_LENGTH = 3;
-
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-const shuffleArray = (array = []) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
 
 const generateType = () => {
   const eventTypes = [
