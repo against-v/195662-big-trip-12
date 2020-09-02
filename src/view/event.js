7be82ca849be4895fb7f5cc573fedf803ed6d0ee
aleksandr <against.vegetables@gmail.com>
2020-09-02 22:23:58 +0300

    поправил офферы в event

--- src/view/event.js
+++ src/view/event.js
@@ -9,7 +9,7 @@ const createOffersTemplate = (items) => {
         ${trimmedItems.map((offer) => {
       return (
         `<li class="event__offer">
-                <span class="event__offer-title">${offer.name}</span>
+                <span class="event__offer-title">${offer.title}</span>
                 &plus;
                 &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
            </li>`
