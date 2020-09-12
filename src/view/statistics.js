65c10395c13fecefcbf9a3be6edb8b61d3ca76b9
aleksandr <against.vegetables@gmail.com>
2020-09-13 01:40:51 +0300

    Установил chart.js, добавил код графиков

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -1,4 +1,142 @@
 import SmartView from "./smart.js";
+import Chart from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+const renderMoneyChart = (moneyCtx) => {
+  return new Chart(moneyCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`✈️ FLY`, `???? STAY`, `???? DRIVE`, `????️ LOOK`, `???? EAT`, `???? RIDE`],
+      datasets: [{
+        data: [400, 300, 200, 160, 150, 100],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: 'end',
+          align: 'start',
+          formatter: (val) => `€ ${val}`
+        }
+      },
+      title: {
+        display: true,
+        text: `MONEY`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+const renderTransportChart = (transportCtx) => {
+  return new Chart(transportCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`???? DRIVE`, `???? RIDE`, `✈️ FLY`, `????️ SAIL`],
+      datasets: [{
+        data: [4, 3, 2, 1],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: 'end',
+          align: 'start',
+          formatter: (val) => `${val}x`
+        }
+      },
+      title: {
+        display: true,
+        text: `TRANSPORT`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+const renderTimeSpendChart = () => {
+
+};
 
 const createStatisticsTemplate = () => {
   return (
@@ -24,11 +162,21 @@ export default class Statistics extends SmartView {
   constructor(events) {
     super();
     this._data = events;
+
+    this._moneyChart = null;
+    this._transportChart = null;
+    this._timeSpendChart = null;
+
     this._setCharts();
   }
 
   removeElement() {
     super.removeElement();
+    if (this._moneyChart !== null || this._transportChart !== null || this._timeSpendChart !== null) {
+      this._moneyChart = null;
+      this._transportChart = null;
+      this._timeSpendChart = null;
+    }
   }
 
   getTemplate() {
@@ -40,6 +188,14 @@ export default class Statistics extends SmartView {
   }
 
   _setCharts() {
-    // Нужно отрисовать два графика
+    if (this._moneyChart !== null || this._transportChart !== null || this._timeSpendChart !== null) {
+      this._moneyChart = null;
+      this._transportChart = null;
+      this._timeSpendChart = null;
+    }
+
+    this._moneyChart = renderMoneyChart();
+    this._transportChart = renderTransportChart();
+    this._timeSpendChart = renderTimeSpendChart();
   }
 }
