a62c09dd2f6f021ad581ef4f1554906154d70fd6
aleksandr <against.vegetables@gmail.com>
2020-09-13 15:18:52 +0300

    поправил ошибку в слове

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -68,14 +68,14 @@ export default class Trip {
   _getEvents() {
     const filterType = this._filterModel.getFilter();
     const events = this._eventsModel.getEvents();
-    const filtredEvents = filter[filterType](events);
+    const filteredEvents = filter[filterType](events);
     switch (this._currentSortType) {
       case SortType.TIME:
-        return groupEventsIntoOneList(filtredEvents.sort(sortByTime));
+        return groupEventsIntoOneList(filteredEvents.sort(sortByTime));
       case SortType.PRICE:
-        return groupEventsIntoOneList(filtredEvents.sort(sortByPrice));
+        return groupEventsIntoOneList(filteredEvents.sort(sortByPrice));
       default:
-        return groupEventsByDay(filtredEvents);
+        return groupEventsByDay(filteredEvents);
     }
   }
 
