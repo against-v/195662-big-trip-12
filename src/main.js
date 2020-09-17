ad3d5a6453102917186442899c09f7c5e770f28f
aleksandr <against.vegetables@gmail.com>
2020-09-17 23:08:04 +0300

    удалил моки

--- src/main.js
+++ src/main.js
@@ -11,55 +11,14 @@ import DestinationsModel from "./model/destinations.js";
 import OffersModel from "./model/offers.js";
 import FilterModel from "./model/filter.js";
 
-
-import {generateEvent} from "./mock/event.js";
-import {generateDestination} from "./mock/destination";
-import {generateOffer} from "./mock/offer";
-
 import Api from "./api.js";
 
 import {render, RenderPosition, remove} from "./utils/render.js";
 
-import {DESTINATIONS, EVENT_TYPES, MenuItem, FilterType, UpdateType} from "./const";
+import {MenuItem, FilterType, UpdateType} from "./const";
 
-const EVENT_COUNT = 10;
 const AUTHORIZATION = `Basic kTy9gIdsz2317rD.`;
 const END_POINT = `https://12.ecmascript.pages.academy/big-trip/`;
-
-const destinations = DESTINATIONS.map((destinationName) => generateDestination(destinationName));
-const offers = EVENT_TYPES.map((offerType) => generateOffer(offerType));
-const events = new Array(EVENT_COUNT).fill().map(() => generateEvent(destinations, offers));
-console.log(`МОКИ`);
-console.log(destinations[0]);
-
-
-const api = new Api(END_POINT, AUTHORIZATION);
-
-api.getDestinations().then((destinations) => {
-  console.log(`Реальные`);
-  console.log(destinations);
-});
-
-api.getOffers().then((offers) => {
-  // console.log(offers[0]);
-});
-
-api.getEvents().then((events) => {
-  // console.log(events);
-  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
-  // а ещё на сервере используется snake_case, а у нас camelCase.
-  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
-  // Есть вариант получше - паттерн "Адаптер"
-});
-
-const destinationsModel = new DestinationsModel();
-const offersModel = new OffersModel();
-const eventsModel = new EventsModel();
-const filterModel = new FilterModel();
-
-destinationsModel.setDestinations(destinations);
-offersModel.setOffers(offers);
-eventsModel.setEvents(events);
 
 const siteHeaderMainElement = document.querySelector(`.trip-main`);
 const siteHeaderControlsElement = siteHeaderMainElement.querySelector(`.trip-controls`);
@@ -67,15 +26,23 @@ const siteMenuTitleElement = siteHeaderControlsElement.querySelector(`h2:first-c
 const siteFilterTitleElement = siteHeaderControlsElement.querySelector(`h2:last-child`);
 const siteMainElement = document.querySelector(`.page-main .page-body__container`);
 
+
+const api = new Api(END_POINT, AUTHORIZATION);
+
+
+const destinationsModel = new DestinationsModel();
+const offersModel = new OffersModel();
+const eventsModel = new EventsModel();
+const filterModel = new FilterModel();
+
+
 const siteMenuComponent = new MenuView(MenuItem.TABLE);
 const addEventButtonComponent = new AddEventButtonView();
 
 const tripPresenter = new TripPresenter(siteMainElement, eventsModel, destinationsModel, offersModel, filterModel);
 const filterPresenter = new FilterPresenter(siteFilterTitleElement, filterModel);
 
-render(siteHeaderMainElement, new InfoView(events), RenderPosition.AFTERBEGIN);
-render(siteMenuTitleElement, siteMenuComponent, RenderPosition.AFTEREND);
-render(siteHeaderMainElement, addEventButtonComponent, RenderPosition.BEFOREEND);
+
 
 let statisticsComponent = null;
 
@@ -113,5 +80,19 @@ const handleAddEventButtonClick = () => {
 siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 addEventButtonComponent.setAddEventButtonClickHandler(handleAddEventButtonClick);
 
+// render(siteHeaderMainElement, new InfoView(events), RenderPosition.AFTERBEGIN);
+render(siteMenuTitleElement, siteMenuComponent, RenderPosition.AFTEREND);
+render(siteHeaderMainElement, addEventButtonComponent, RenderPosition.BEFOREEND);
+
 filterPresenter.init();
 tripPresenter.init();
+
+api.getEvents().then((events) => {
+  eventsModel.setEvents(events);
+});
+api.getDestinations().then((destinations) => {
+  destinationsModel.setDestinations(destinations);
+});
+api.getOffers().then((offers) => {
+  offersModel.setOffers(offers);
+});
