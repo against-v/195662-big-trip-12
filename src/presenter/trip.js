dc95f85aa6828488d8cf34aeb707e46f04093b71
aleksandr <against.vegetables@gmail.com>
2020-09-20 02:35:39 +0300

    обновляю состояние перед отправкой на сервер

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -6,7 +6,7 @@ import EventsListView from "../view/events-list.js";
 import LoadingView from "../view/loading.js";
 import NoEventView from "../view/no-event.js";
 
-import EventPresenter from "./event.js";
+import EventPresenter, {State as EventPresenterViewState} from "./event.js";
 import EventNewPresenter from "./event-new.js";
 import {render, RenderPosition, remove} from "../utils/render.js";
 import {filter} from "../utils/filter.js";
@@ -107,16 +107,19 @@ export default class Trip {
   _handleViewAction(actionType, updateType, update) {
     switch (actionType) {
       case (UserAction.UPDATE_EVENT):
+        this._eventPresenter[update.id].setViewState(EventPresenterViewState.SAVING);
         this._api.updateEvent(update).then((response) => {
           this._eventsModel.updateEvent(updateType, response);
         });
         break;
       case (UserAction.ADD_EVENT):
+        this._eventNewPresenter.setSaving();
         this._api.addEvent(update).then((response) => {
           this._eventsModel.addEvent(updateType, response);
         });
         break;
       case (UserAction.DELETE_EVENT):
+        this._eventPresenter[update.id].setViewState(EventPresenterViewState.DELETING);
         this._api.deleteEvent(update).then(() => {
           this._eventsModel.deleteEvent(updateType, update);
         });
