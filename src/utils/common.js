749553cb102b0297dd765e75b4768da41992930a
aleksandr <against.vegetables@gmail.com>
2020-09-22 23:54:47 +0300

    Б18

--- src/utils/common.js
+++ src/utils/common.js
@@ -24,20 +24,6 @@ export const humanizeDuration = (duration) => {
   return `${minutes}M`;
 };
 
-export const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-export const shuffleArray = (array = []) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
-
 export const capitalizeString = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
